c9bad36f288bbda99b0f6fb04b803f99fd0363be
pawnpawee <keeppmymmr.22@gmail.com>
2026-05-21 00:37:23 +0700

    animation

--- data/constants.ts
+++ data/constants.ts
@@ -387,7 +387,7 @@ export const designProjectsData = [
     title: "XiaoFuMeow Web Application PROTOTYPE",
     description:
       "a responsive food e-commerce website prototype for XiaoFuMeow, designed with Figma.",
-    skills: [],
+    skills: ["Figma", "UI/UX Design"],
     videoEmbedUrl:
       "https://embed.figma.com/design/3QpBu4nnjLjzwZWHzkDIAk/Final_WebCMM349?node-id=0-1&embed-host=share",
     year: "2024",
@@ -398,7 +398,7 @@ export const designProjectsData = [
     title: "PROTOTYPE MU(x) Gamification",
     description:
       "Engineered a strategic solution to enhance student engagement for Mahidol University’s online learning platform (MUx) via gamification, reaching the Final 11 teams nationwide.",
-    skills: [],
+    skills: ["Figma", "UI/UX Design"],
     videoEmbedUrl:
       "https://www.figma.com/embed?embed_host=share&amp;url=https%3A%2F%2Fwww.figma.com%2Fproto%2FdR3qcrXMQl5ha5e95VdxUr%2F%25E0%25B9%2580%25E0%25B8%25A3%25E0%25B8%25B5%25E0%25B8%25A2%25E0%25B8%2599%25E0%25B8%2581%25E0%25B8%25B1%25E0%25B8%2599%25E0%25B8%2595%25E0%25B9%2588%25E0%25B8%25AD%3Ftype%3Ddesign%26node-id%3D152-174%26t%3D2OboEdxkMpoKQX0K-1%26scaling%3Dscale-down%26page-id%3D0%253A1%26starting-point-node-id%3D152%253A174%26mode%3Ddesign",
     year: "2024",
@@ -408,7 +408,7 @@ export const designProjectsData = [
     title: "PORTFOLIO APPLICATION PROTOTYPE",
     description:
       "a responsive portfolio application prototype, designed with Figma.",
-    skills: [],
+    skills: ["Figma", "UI/UX Design"],
     videoEmbedUrl:
       "https://www.figma.com/embed?embed_host=share&amp;url=https%3A%2F%2Fwww.figma.com%2Fproto%2FcdjK5qxohIeTLw7Kbr9tRu%2FPortfolio%3Ftype%3Ddesign%26node-id%3D67-134%26t%3DjhkDDtsV9oyweQlc-1%26scaling%3Dscale-down%26page-id%3D0%253A1%26mode%3Ddesign",
     year: "2024",
@@ -418,7 +418,7 @@ export const designProjectsData = [
     title: "SE-ED WEBSITE PROTOTYPE",
     description:
       "a responsive e-commerce website prototype for SE-ED, designed with Figma.",
-    skills: [],
+    skills: ["Figma", "UI/UX Design"],
     videoEmbedUrl:
       "https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Fproto%2F5ftLKlHvSUm788CijgTY8p%2FSE-ED-Prototype%3Ftype%3Ddesign%26node-id%3D1-16158%26t%3DaWtz5exxaLriDI9I-1%26scaling%3Dscale-down%26page-id%3D0%253A1%26starting-point-node-id%3D1%253A16158%26mode%3Ddesign",
     year: "2024",
@@ -428,7 +428,7 @@ export const designProjectsData = [
     title: "EKKAMAI TOUR APPLICATION",
     description:
       "a responsive travel application prototype for EKKAMAI TOUR, designed with Figma.",
-    skills: [],
+    skills: ["Figma", "UI/UX Design"],
     videoEmbedUrl:
       "https://www.figma.com/embed?embed_host=share&amp;url=https%3A%2F%2Fwww.figma.com%2Fproto%2FZMoE0a67LdiLHdywy6TZdd%2FEkamai%3Fnode-id%3D41-120%26scaling%3Dscale-down%26mode%3Ddesign%26t%3DgTTyWwGTdLKWhDAA-1",
     year: "2024",
@@ -438,7 +438,7 @@ export const designProjectsData = [
     title: "Res-Q Mobile Application",
     description:
       "a responsive mobile application about SDG principles, designed with Figma.",
-    skills: [],
+    skills: ["Figma", "UI/UX Design"],
     videoEmbedUrl:
       "https://embed.figma.com/design/EHX1WRB4uZAbRPhPn4k2Z6/323?node-id=0-1&embed-host=share",
     year: "2025",
@@ -448,7 +448,7 @@ export const designProjectsData = [
     title: "Ikigai Website",
     description:
       "a responsive website for the Ikigai project, designed with Figma.",
-    skills: [],
+    skills: ["Figma", "UI/UX Design"],
     videoEmbedUrl:
       "https://embed.figma.com/design/vbwVAkvvTgrPIM6GpOM0t0/Thesis-Ikigai-Website?node-id=0-1&embed-host=share",
     year: "2026",
@@ -458,7 +458,7 @@ export const designProjectsData = [
     title: "Portfolio Website 2026",
     description:
       "a responsive website for the Portfolio 2026 project, designed with Figma.",
-    skills: [],
+    skills: ["Figma", "UI/UX Design"],
     videoEmbedUrl:
       "https://embed.figma.com/design/Ogl3QaSe7zf6AMg7LDah19/Portfolio?node-id=0-1&embed-host=share",
     year: "2026",
@@ -468,13 +468,94 @@ export const designProjectsData = [
     title: "Cartoon Club Website",
     description:
       "a responsive website for the Cartoon Club project, designed with Figma.",
-    skills: [],
+    skills: ["Figma", "UI/UX Design"],
     videoEmbedUrl:
       "https://embed.figma.com/design/OcvnmDKqWXtSDrsQiUAz0h/420-Final?node-id=0-1&embed-host=share",
     year: "2026",
   },
 ];
-export const animationProjectsData = [];
+export const animationProjectsData = [
+  {
+    id: "loop-motion",
+    title: "LOOP MOTION",
+    skills: ["After Effects", "Motion Graphics", "Iillustrator"],
+    videoEmbedUrl:
+      "https://www.youtube.com/embed/ekPU4Mp8beg?si=U-FpHPwIEpcNwaNs",
+    year: "2023",
+  },
+  {
+    id: "pinball-animation",
+    title: "Pinball Animation",
+    skills: ["After Effects", "Motion Graphics", "Iillustrator"],
+    videoEmbedUrl:
+      "https://www.youtube.com/embed/7jlDcVUg3Nc?si=SR3Q8pZUZGfvNzdq",
+    year: "2023",
+  },
+  {
+    id: "pen-tablet-ads",
+    title: "Pen Tablet Ads Prism - superior pro",
+    skills: ["After Effects", "Motion Graphics", "Iillustrator"],
+    videoEmbedUrl:
+      "https://www.youtube.com/embed/EfR8iWBvd5w?si=BvDAX3J7RUUnsvq_",
+    year: "2023",
+  },
+  {
+    id: "lyrics-animation",
+    title: "Lyrics Animation | Hayloft II - Mother Mother",
+    skills: ["After Effects", "Motion Graphics", "Iillustrator"],
+    videoEmbedUrl:
+      "https://www.youtube.com/embed/lhs11nxfWEc?si=g8QVS2137ydXXqPZ",
+    year: "2023",
+  },
+  {
+    id: "parent-animation",
+    title: "Parent Animation - Factory Tour (Powerpuff Girls)",
+    skills: ["After Effects", "Motion Graphics", "Iillustrator"],
+    videoEmbedUrl:
+      "https://www.youtube.com/embed/yfa3UFkQBEA?si=SIn2TQM68vu7zJeF",
+    year: "2023",
+  },
+  {
+    id: "airway-animation",
+    title: "Motion Airway",
+    skills: ["After Effects", "Motion Graphics", "Iillustrator"],
+    videoEmbedUrl:
+      "https://www.youtube.com/embed/1s5IjwyLcvI?si=jHf5aYRp4x6DyQC2",
+    year: "2023",
+  },
+  {
+    id: "bouncing-ball-animation",
+    title: "Bouncing Ball 3D Animation",
+    skills: ["3d Graphics", "Maya"],
+    videoEmbedUrl:
+      "https://www.youtube.com/embed/Hu_lhdWUKdw?si=--yuDgSBd2d1wL1K",
+    year: "2024",
+  },
+  {
+    id: "bouncing-ball-animation-action",
+    title: "Bouncing Ball 3D Animation Action Camera",
+    skills: ["3d Graphics", "Maya"],
+    videoEmbedUrl:
+      "https://www.youtube.com/embed/A1WpFtC3l3c?si=TXifVmONVSZqrEpd",
+    year: "2024",
+  },
+  {
+    id: "fantasy-theme-island",
+    title: "Fantasy Theme Island (Loop animation)",
+    skills: ["3d Graphics", "Maya"],
+    videoEmbedUrl:
+      "https://www.youtube.com/embed/Ox7iPxRhbTc?si=LcMeP--DIMYB0W2B",
+    year: "2024",
+  },
+  {
+    id: "curse-revence-animation",
+    title: "Curse of the Revence - Teaser Game",
+    skills: ["3d Graphics", "Maya"],
+    videoEmbedUrl:
+      "https://www.youtube.com/embed/RQqLQhZf4Wo?si=bCk1yvG1oZEN9QRm",
+    year: "2025",
+  },
+];
 export const renderingProjectsData = [];
 export const productionProjectsData = [];
 
